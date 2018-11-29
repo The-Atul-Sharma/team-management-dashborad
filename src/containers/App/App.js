fb0553d737e4a98e26d2a170869c801f16c75c31
Atul Sharma <theatsharma@gmail.com>
2018-11-30 00:51:56 +0530

    Set folder structure for application

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Team management dashboard
+      </div>
+    );
+  }
+}
+
+export default App;
